9edfd7ec6d5632056fc75de48567cf0df0a3d0cb
stan <stan@onemob.com>
2018-12-06 13:09:34 -0800

    add Gallery to Product

--- app/javascript/src/components/views/Product/Gallery/index.jsx
+++ app/javascript/src/components/views/Product/Gallery/index.jsx
@@ -2,50 +2,10 @@ import React from "react";
 
 import "./gallery.scss";
 
-class Gallery extends React.Component {
-  constructor(props) {
-    super(props);
+import Gallery from "src/components/shared/Gallery";
 
-    this.state = {
-      activeImageUrl: this.props.images[0].url
-    };
+const GalleryContainer = (props) => (
+  <Gallery images={props.images} productId={props.productId} />
+);
 
-    this.changeActiveImage = this.changeActiveImage.bind(this);
-  }
-
-  changeActiveImage(url) {
-    this.setState({ activeImageUrl: url });
-  }
-
-  isActiveImage(url) {
-    return url === this.state.activeImageUrl;
-  }
-
-  render() {
-    const images = this.props.images;
-    const { activeImageUrl } = this.state;
-
-    return (
-      <div id="gallery">
-        <div>
-          <img src={activeImageUrl} alt="" className="active-image"/>
-        </div>
-        <div>
-          {
-            images.map(({ url, id }) => (
-              <img
-                src={url}
-                alt=""
-                key={id}
-                className={this.isActiveImage(url) ? "thumbnail active-thumbnail" : "thumbnail"}
-                onClick={() => this.changeActiveImage(url)}
-              />
-            ))
-          }
-        </div>
-      </div>
-    )
-  }
-}
-
-export default Gallery;
+export default GalleryContainer;
