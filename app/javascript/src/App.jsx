affabb0f0818ac12e92645a23f9feec2d7be301d
stan <stan@onemob.com>
2018-12-12 16:49:43 -0800

    add store

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import routes from "src/routes"
 import Menu from "src/components/Menu"
 import CartContainer from "src/components/CartContainer"
 import ProductImages from "src/routes/product-images"
+import store from "src/store";
 
 class App extends React.Component {
   render() {
     return (
-      <CartContainer>
-        <Router basename="/app">
-          <div className="container">
-            <Menu />
-            <Switch>
-              {
-                routes.map((route, index) => (
-                  <Route {...route} key={index} />
-                ))
-              }
-            </Switch>
-            <Route {...ProductImages}/>
-          </div>
-        </Router>
-      </CartContainer>
+      <Provider store={store}>
+        <CartContainer>
+          <Router basename="/app">
+            <div className="container">
+              <Menu />
+              <Switch>
+                {
+                  routes.map((route, index) => (
+                    <Route {...route} key={index} />
+                  ))
+                }
+              </Switch>
+              <Route {...ProductImages}/>
+            </div>
+          </Router>
+        </CartContainer>
+      </Provider>
     )
   }
 }
