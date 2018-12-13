854020125a2b6de03380a0aaf894d7c41ea05c7b
stan <stan@onemob.com>
2018-12-12 16:51:44 -0800

    move products to redux store

---
+++ app/javascript/src/reducers/Products.js
@@ -0,0 +1,20 @@
+import * as types from "src/constants/actionTypes/ProductsActionTypes";
+
+const initialState = {
+  isFetching: false,
+  error: false,
+  entries: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case types.FETCH_PRODUCTS_REQUEST:
+      return Object.assign({}, state, { isFetching: true });
+    case types.FETCH_PRODUCTS_ERROR:
+      return Object.assign({}, state, { error: true });
+    case types.FETCH_PRODUCTS_SUCCESS:
+      return Object.assign({}, state, { entries: action.entries });
+    default:
+      return state;
+  }
+}
---
+++ app/javascript/src/reducers/index.js
@@ -0,0 +1,5 @@
+import products from "./Products";
+
+export default {
+  products
+};
